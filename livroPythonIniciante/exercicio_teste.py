1a3a94bf9935ca649982b9a609a758c6d09fb25f
LuizMoreira-py <luizmoreira98@gmail.com>
2021-03-21 13:23:13 -0300

    Update exercicio_teste.py

---
+++ livroPythonIniciante/exercicio_teste.py
@@ -0,0 +1,9 @@
+velocidadeCarro = int(input("informe a velocidade do carro: "))
+multa = float(5)
+
+if velocidadeCarro > 80:
+    exedente = velocidadeCarro - 80
+    valorMulta = exedente * multa
+
+    print("Você foi multado!")
+    print("O valor da multa é R$ {:5.2f}". format(valorMulta))
