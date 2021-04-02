89fbe2baa8ab701ba768e4b2e88eb21897c5bf27
LuizMoreira-py <luizmoreira98@gmail.com>
2021-04-02 11:38:55 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,7 +1,16 @@
-idade = int(input("Digite a idade de seu carro: "))
+# Escreva um programa que pergunte a distância
+# que um passageiro deseja percorrer em km.
+# Calcule o preço da passagem, cobrando R$ 0,50 por km para
+# viagens de até de 200 km, e R$ 0,45 para viagens mais longas.
 
-if idade <= 3:
-    print("Seu carro é novo")
+distracia = float(input("Informe a distância(KM) que deseja percorrer: "))
+preco = 0
+
+if distracia <= 200:
+    preco = 0.50
 
 else:
-    print("Seu carro é velho")
+    preco = 0.45
+
+custo = distracia * preco
+print(f"Valor total: R$ {custo:6.2f}")
