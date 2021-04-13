c0b947ddfdb4f635fa3c44118f640270c1796421
LuizMoreira-py <luizmoreira98@gmail.com>
2021-04-12 21:50:26 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,26 +1,4 @@
-consumo = int(input("Informe o consumo(kWh) de energia: "))
-print("Informe o tipo de instalação:")
-instalacao = (input("Residencial(R), Indrustrial(I) e Comercial(C)"))
-
-if instalacao == "R":
-    if consumo <= 500:
-        valor = consumo * 0.756221
-    elif consumo > 500:
-        valor = consumo * 0.65
-
-elif instalacao == "I":
-    if consumo <= 1000:
-        valor = consumo * 0.55
-    elif consumo > 1000:
-        valor = consumo * 0.60
-
-elif instalacao == "C":
-    if consumo <= 5000:
-        valor = consumo * 0.756221
-    elif consumo > 5000:
-        valor = consumo * 0.60
-
-else:
-    print("Valor invalido!")
-
-print(f"O valor a pagar R$ {valor:6.2f}")
+x = 1
+while x <= 100:
+    print(x)
+    x = x + 1
