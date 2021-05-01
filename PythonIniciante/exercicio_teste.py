c2959ab1d5950b348c89f2e19c8d79785a718643
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-01 12:48:32 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,10 +1,7 @@
-n1 = int(input("Informe um dividendo: "))
-n2 = int(input(f"Informe um divisor para o número {n1} / "))
-
-div = n1
-cont = 0
-
-while div > 0:
-    div -= n2
-    cont += 1
-    print(f"Divisão {cont}: {div}")
+n = 1
+soma = 0
+while n <= 10:
+    x = int(input(f"Digite o {n} número: "))
+    soma = soma + x
+    n += 1
+print(f"Soma: {soma}")
