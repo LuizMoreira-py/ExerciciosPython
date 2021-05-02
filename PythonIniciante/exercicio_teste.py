dc19cfecbda64531d3f2c85747a72d8c70e813f9
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-02 19:31:33 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,12 +1,12 @@
 mes = 0
 
-deposito = float(input())
-taxa = float(input())
+deposito = float(input("Valor do depósito: R$ "))
+taxa = float(input("Valor da taxa(%): "))
 
 rendimento = deposito * taxa
 
 while mes <= 23:
     rendimentoPeriodo = (deposito * taxa) * mes
-    rendiimentoTotal = deposito + rendimentoPeriodo
+    rendimentoTotal = deposito + rendimentoPeriodo
     mes += 1
-    print(f"Mês {mes} ---- Depósito: R$ {deposito:6.2f} ---- Rendimento: R$ {rendimento:6.2f} ---- Rendimento periodo: R$ {rendimentoPeriodo:6.2f} ---- Rendimento total: {rendiimentoTotal:6.2f}")
+    print(f"Mês {mes} ---- Depósito: R$ {deposito:6.2f} ---- Rendimento: R$ {rendimento:6.2f} ---- Rendimento periodo: R$ {rendimentoPeriodo:6.2f} ---- Rendimento total: {rendimentoTotal:6.2f}")
