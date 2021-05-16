239270aa3c866187443ace7af1e923954a2f2276
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-16 15:47:54 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -2,15 +2,18 @@
 # ler os números até que o usuário digite 0 (zero). No final da execução,
 # exiba a quantidade de números digitados como a soma e a média aritmética.
 
-contador = 0
 soma = 0
+contador = 0
+media = 0
+
 while True:
-    numeros = int(input("Digitite um número inteiro: "))
+    numeros = int(input("Digite um número inteiro: "))
     if numeros == 0:
         break
     soma += numeros
-    
     contador += 1
+media = soma // contador
 
 print(f"Total de números digitados: {contador}")
-print(f"Soma: {soma}")
+print(f"Soma dos números digitados: {soma}")
+print(f"Média dos números digitados: {media}")
