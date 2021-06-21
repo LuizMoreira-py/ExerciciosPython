4b562f82aa89a15041239f742194f6085d123750
LuizMoreira-py <luizmoreira98@gmail.com>
2021-06-20 22:04:32 -0300

    commit

--- Udemy/Luiz/learnerPython.py
+++ Udemy/Luiz/learnerPython.py
@@ -9,11 +9,25 @@ if numero.isdigit():
 else:
     print("O valor digitado não é inteiro!")
 
-horas = int(input("Digite somente a hora do seu relógiio: "))
+horas = input("Digite somente a hora do seu relógio: ")
 
-if horas <= 11:
-    print("Bom dia!")
-elif horas >= 12 and horas <= 17:
-    print("Boa tarde!")
+if horas.isdigit():
+    horas = int(horas)
+    if horas <= 11:
+        print("Bom dia!")
+    elif horas >= 12 and horas <= 17:
+        print("Boa tarde!")
+    elif horas >= 18 and horas <= 23:
+        print("Boa noite!")
+    else:
+        print("Valor invalido")
+
+nome = input("Digite o seu nome: ")
+caracteres = len(nome)
+
+if caracteres <= 4:
+    print("Seu nome é muito curto!")
+elif caracteres > 4 and caracteres <= 6:
+    print("Seu nome é normal!")
 else:
-    print("Boa noite!")
+    print("Seu nome é muito grande!")
