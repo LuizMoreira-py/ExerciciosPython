bd320daa1653cc30899a4ebb9c3c7490147a4477
LuizMoreira-py <luizmoreira98@gmail.com>
2021-06-20 21:24:14 -0300

    commit

--- Udemy/Luiz/learnerPython.py
+++ Udemy/Luiz/learnerPython.py
@@ -1,9 +1,13 @@
-num1 = int(input("Digite um número: "))
-num2 = int(input("Digite outro número: "))
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
 
+if num1.isdigit() and num2.isdigit():
+    print("Os números digitados são inteiros, pode somar")
+    num1 = int(num1)
+    num2 = int(num2)
 
-def soma(num1, num2):
-    return num1 + num2
-
-
-print(soma(num1, num2))
+    def soma(num1, num2):
+        return num1 + num2
+    print(soma(num1, num2))
+else:
+    print("Não pode converter os números")
