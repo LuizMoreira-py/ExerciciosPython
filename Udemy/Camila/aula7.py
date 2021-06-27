5ae989f73a6ff7961829b5e456d964435cf6b3f5
LuizMoreira-py <luizmoreira98@gmail.com>
2021-06-27 13:49:34 -0300

    commit

--- Udemy/Camila/aula7.py
+++ Udemy/Camila/aula7.py
@@ -15,5 +15,3 @@ print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
 print('{0} tem {1} anos de idade e seu imc é {2}' .format(nome, idade, imc))
 
 print('{n} tem {i} anos e seu imc é {im}'.format(n=nome, i=idade, im=imc))
-
-
